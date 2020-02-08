3c6f17265f91e9995c816edb0f33bcf6bd10f4bf
Stiven Trujillo <strujillo1293@gmail.com>
2020-02-07 20:17:49 -0500

    added cors

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const MONGO = require('./const');
 const router = require('./routes/routes');
+const cors = require('cors');
 const PORT = process.env.PORT || 4200;
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 
 mongoose.connect(MONGO.URI, { useNewUrlParser: true, useUnifiedTopology: true  })
